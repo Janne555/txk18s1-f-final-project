47d7c9d4f87482ae5b5066c5ee8684222e426c76
emilbesic <43534058+emilbesic@users.noreply.github.com>
2018-09-25 12:37:37 +0300

    lisätty password kohta

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -6,10 +6,22 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {attribution: '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(karttani);
 
-function isValid(myNode){
-  var password = myNode.value;
-  if (password == "rawr")
-  {alert('Correct!')}
-  else
-  {alert('Wrong Password')}
+function passWord() {
+  var testV = 1;
+  var pass1 = prompt('Please Enter Your Password',' ');
+  while (testV < 3) {
+    if (!pass1)
+      history.go(-1);
+    if (pass1.toLowerCase() == "letmein") {
+      alert('You Got it Right!');
+      window.open('www.wikihow.com');
+      break;
+    }
+    testV+=1;
+    var pass1 =
+        prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+  }
+  if (pass1.toLowerCase()!="password" & testV ==3)
+    history.go(-1);
+  return " ";
 }
