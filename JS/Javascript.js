cfbd2895a9a5d5e79b4857dac84141be73609deb
emilbesic <43534058+emilbesic@users.noreply.github.com>
2018-09-25 12:25:58 +0300

    lisätty password kohta

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -5,3 +5,11 @@ let karttani = L.map('kartta').setView([long, lat], 16);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {attribution: '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(karttani);
+
+function isValid(myNode){
+  var password = myNode.value;
+  if (password == "rawr")
+  {alert('Correct!')}
+  else
+  {alert('Wrong Password')}
+}
