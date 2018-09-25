a17a8f7a564efa6cc305dfa3f2c8781398f79b19
ankkahh <43534031+ankkahh@users.noreply.github.com>
2018-09-25 12:31:39 +0300

    Lisätty karttaan ominaisuus

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,7 +1,15 @@
 let long, lat;
-long = 60.17;
-lat = 24.93;
+long = 60.163;
+lat = 24.931;
 let karttani = L.map('kartta').setView([long, lat], 16);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {attribution: '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(karttani);
+let popup = L.popup();
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("Klikkasit karttaa " + e.latlng.toString())
+            .openOn(karttani);
+    }
+    karttani.on('click', onMapClick);
