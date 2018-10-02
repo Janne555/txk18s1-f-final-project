0d79df32a0e400c83f25e57651d4938d111cd749
Janne555 <Janne555@users.noreply.github.com>
2018-10-02 11:17:59 +0300

    tiedostoja lisätty

---
+++ JS/slideshow.js
@@ -0,0 +1,47 @@
+//define constants
+const pictures = [
+    "kuvat/assorted-assortment-baked-1236680.jpg",
+    "kuvat/bake-chocolate-confectionery-1028708.jpg",
+    "kuvat/baked-goods-colorful-colourful-14105.jpg",
+    "kuvat/cake-cupcake-cupcakes-585581.jpg",
+    "kuvat/chocolate-cupcakes-chocolate-muffins-cupcakes-93451.jpg"];
+
+const slideshow_button_next = document.getElementById("slideshow_button_next");
+const slideshow_button_prev = document.getElementById("slideshow_button_prev");
+const current_picture = document.getElementById("current_picture");
+
+//add listeners
+slideshow_button_next.addEventListener("click", function (evt) {
+    nextSlide();
+})
+
+slideshow_button_prev.addEventListener("click", function (evt) {
+    prevSlide();
+})
+
+
+var currentSlide = 0;
+var maxSlides = 4;
+
+function nextSlide() {
+    if (currentSlide === maxSlides) {
+        showSlide(0);
+        return;
+    } else {
+        showSlide(currentSlide += 1);
+    }
+}
+
+function prevSlide() {
+    if (currentSlide === 0) {
+        showSlide(maxSlides)
+        return;
+    } else {
+        showSlide(currentSlide -= 1);
+    }
+}
+
+function showSlide(n) {
+    current_picture.setAttribute("src", pictures[n]);
+    currentSlide = n;
+}
