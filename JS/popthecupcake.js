c383cc40bf0d76d295431c3a1284aa6bf95f64ca
Janne555 <Janne555@users.noreply.github.com>
2018-10-09 10:24:18 +0300

    animaatiota kuppikakkupeliin

--- JS/popthecupcake.js
+++ JS/popthecupcake.js
@@ -6,21 +6,24 @@ var width = 500 - 60;
 var height = 500 - 60;
 
 function pop() {
-    console.log("popping");
     let x = Math.floor(Math.random() * width);
     let y = Math.floor(Math.random() * height);
 
     console.log("x: " + x + " y: " + y);
-    cupcake.style.display = "none";
+    cupcake.classList.add("popped");
+    cupcake.classList.remove("appear");
+
 
     points += 1;
 
-    cupcake.style.top = "" + y + "px";
-    cupcake.style.left = "" + x + "px";
+
     setTimeout(function (evt) {
-        cupcake.style.display = "block";
-        console.log("popped");
-    }, 500);
+        cupcake.style.top = "" + y + "px";
+        cupcake.style.left = "" + x + "px";
+        cupcake.classList.add("appear");
+        cupcake.classList.remove("popped");
+
+    }, 600);
 
     score.innerText = points;
 }
